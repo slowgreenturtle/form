9547a62f17816b270e9e31f56f6b13ea491cedfb
Tony Black <tonyblack@outlook.com>
2018-03-22 19:08:07 -0600

    Added service provider.

---
+++ defaults/views/form/default/input.blade.php
@@ -0,0 +1,20 @@
+<div class="{{ $div_classes }}" id="{{ $div_name}}">
+        {!! $label !!}
+    @if($prepend_text != null || $append_text != null)
+        <div class="input-group">
+            @endif
+
+            @if($prepend_text != null)
+                <span class=\"input-group-addon\">{{$prepend_text}}</span>
+            @endif
+
+            {{ $form_element }}
+
+            @if($append_text != null)
+                <span class=\"input-group-addon\">$append_text</span>
+            @endif
+
+            @if($prepend_text != null || $append_text != null)
+        </div>
+    @endif
+</div>
---
+++ src/SGTServiceProvider.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace LaravelCaptcha\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Validator;
+
+class SGTServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        $this->publishConfigFile();
+        $this->publishViewFiles();
+    }
+
+    /**
+     * Publish captcha.php file.
+     *
+     * @return void
+     */
+    public function publishConfigFile()
+    {
+
+        if (method_exists($this, 'publishes'))
+        {
+            $this->publishes([
+                __DIR__ . '/../defaults/sgtform.php' => config_path('sgtform.php')
+            ]);
+        }
+    }
+
+    public function publicViewFiles()
+    {
+
+        $view_path = __DIR__ . '/../defaults/views';
+
+        $this->loadViewsFrom($view_path, 'sgtform');
+
+        $this->publishes([$view_path => resource_path('views/vendor/sgtform')]);
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+
+        return [];
+    }
+
+}
