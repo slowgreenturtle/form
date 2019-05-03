7ff6ca020d907a8b1510b411b09545767dd3b3c1
Tony Black <tonyblack@outlook.com>
2019-05-02 19:37:56 -0600

    Added table.

---
+++ src/Table/DataTable/Search.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SGT\Table\DataTable;
+
+class Search
+{
+
+    public $start        = 0;
+    public $limit        = 0;
+    public $column       = -1;
+    public $sort_order   = 'ASC';
+    public $search_value = '';
+
+    public function fill($request, $custom_search_fields = [])
+    {
+
+        $this->start        = $request->input('start');
+        $this->limit        = $request->input('length');
+        $this->search_value = $request->input('search.value');
+        $this->column       = $request->input('order.0.column');
+        $this->sort_order   = $request->input('order.0.dir');
+
+        foreach ($custom_search_fields as $field)
+        {
+            $this->$field = $request->input($field);
+        }
+    }
+
+}
---
+++ src/Table/Simple.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Html\Table;
+
+
+class Simple
+{
+
+    public function headers()
+    {
+
+        return $this->headers;
+    }
+}
