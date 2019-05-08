dbc9fd2265d24eb3fd5e3ea36c08ec6ea2644af0
Tony Black <tonyblack@outlook.com>
2019-05-08 17:33:52 -0600

    Added bootstrap tables.

--- defaults/views/bootstrap/4/table/datatable/default.blade.php
+++ defaults/views/bootstrap/4/table/datatable/default.blade.php
@@ -1,50 +1,38 @@
-<div class="dataTable_wrapper">
+<table class="{{ $table->htmlClass('table') }}"
+       id="{{$table->name()}}" role="grid" aria-describedby="{{ $table->name()}}_info">
+    <thead>
 
-    <div id="{{ $table->name('wrapper') }}" class="{{ $table->htmlClass('wrapper') }}">
-
-        <div class="row">
-
-            <div class="col-sm-12">
-                <table class="{{ $table->htmlClass('table') }}"
-                       id="{{$table->name()}}" role="grid" aria-describedby="{{ $table->name()}}_info">
-                    <thead>
-
-                    <tr class="{{ $table->htmlClass('row') }}" role="row">
-
-                        @foreach($table->headers() as $header)
-
-                            <th>
+    <tr class="{{ $table->htmlClass('row') }}" role="row">
+        @foreach($table->headers() as $header)
+            <th>
                                 <span data-toggle="tooltip" data-placement="bottom"
                                       title="{{$header['tooltip']}}">
 
                                 {{ $header['name'] }}
 
                                 </span>
-                            </th>
+            </th>
 
-                        @endforeach
+        @endforeach
 
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {!! $table->body() !!}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-</div>
-
+    </tr>
+    </thead>
+    <tbody>
+    {!! $table->body() !!}
+    </tbody>
+</table>
 
 @section('scripts')
 
     @parent
 
     <script>
-        $(document).ready(function () {
+        $(document).ready(function ()
+        {
 
             var settings = @json($table->jsSettings());
-            if (settings.ajax.data) {
+            if (settings.ajax.data)
+            {
                 settings.ajax.data = window[settings.ajax.data];
             }
 
