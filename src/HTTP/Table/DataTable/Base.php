d4cd5bfee7ca30cbd9c2720a9ff6ffd09d6f6f02
Tony Black <tonyblack@outlook.com>
2019-07-30 12:43:47 -0600

    Fixed tables not using ajax.

--- src/HTTP/Table/DataTable/Base.php
+++ src/HTTP/Table/DataTable/Base.php
@@ -280,7 +280,9 @@ abstract class Base
 
         $html = '';
 
-        $records = $this->records();
+        $data = $this->data();
+
+        $records = Arr::get($data, 'data');
 
         $row_class = $this->htmlClass('row');
 
@@ -333,6 +335,215 @@ abstract class Base
     {
 
         $this->data_url = $url;
+
+    }
+
+    public function data()
+    {
+
+        $record_count        = 0;
+        $record_filter_count = 0;
+
+        #   Get the raw query records
+        $query = $this->query();
+
+        if ($query)
+        {
+            $record_count = $query->count();
+        }
+
+        # append the search query
+        $query = $this->querySearch($query);
+        $query = $this->querySetOrder($query);
+
+        if ($query)
+        {
+            $record_filter_count = $query->count();
+        }
+
+        $query = $this->queryLimit($query);
+
+        # take the search records and return the formatted result
+        $records = $this->querySearchRecords($query);
+
+        # take the formatted results and create the field elements.
+        $results = $this->formattedRecords($records);
+
+        $data['data']            = $results;
+        $data['recordsTotal']    = $record_count;
+        $data['recordsFiltered'] = $record_filter_count;
+
+        return $data;
+    }
+
+    public function query()
+    {
+
+
+        return null;
+    }
+
+    /**
+     * @param $query
+     *              Return the query with the search filters appended to it.
+     * @return mixed
+     */
+    public function querySearch($query)
+    {
+
+
+        return $query;
+
+    }
+
+    public function querySetOrder($query)
+    {
+
+        $sort_directions = [
+            'asc'  => 'asc',
+            'desc' => 'desc'
+        ];
+
+        $order = $this->search->order;
+
+        foreach ($order as $order_item)
+        {
+
+            $column_number = Arr::get($order_item, 'column', '');
+
+            if (is_numeric($column_number))
+            {
+
+                $order_column = Arr::get($this->search->columns, $column_number);
+
+                if ($order_column)
+                {
+
+                    $column_name = Arr::get($order_column, 'name');
+
+                    if (empty($column_name))
+                    {
+                        continue;
+                    }
+
+                    $sort_direction = Arr::get($order_item, 'dir', 'asc');
+
+                    $sort_direction = Arr::get($sort_directions, $sort_direction, 'asc');
+
+                    $orderMethodName = 'order_' . $column_name;
+
+                    if (method_exists($this, $orderMethodName))
+                    {
+                        $content = $this->$orderMethodName($query, $sort_direction);
+                    }
+                    else
+                    {
+
+                        $column = $this->column($column_name);
+
+                        if ($column)
+                        {
+                            $sort_column = Arr::get($column, 'sort_field');
+
+                            if ($sort_column)
+                            {
+                                $query->orderBy($sort_column, $sort_direction);
+                            }
+
+                        }
+                    }
+                }
+            }
+
+        }
+
+        return $query;
+    }
+
+    public function column($column_name)
+    {
+
+        return Arr::get($this->tableColumns(), $column_name);
+    }
+
+    public function queryLimit($query)
+    {
+
+        if ($query == null)
+        {
+            return $query;
+        }
+
+        $limit = $this->search->limit;
+
+        if (!empty($limit))
+        {
+            $query->limit($limit);
+        }
+
+        $start = $this->search->start;
+
+        if (!empty($start))
+        {
+            $query->offset($start);
+        }
+
+        return $query;
+
+    }
+
+    /**
+     * Retrieve the list of records used for this display, filtered by search variables.
+     *
+     * @return array
+     */
+    public function querySearchRecords($query_search)
+    {
+
+        if ($query_search)
+        {
+            return $query_search->get();
+        }
+
+        return [];
+    }
+
+    /**
+     * Retrieve the records formatted by their column_* calls.
+     *
+     * @return array
+     */
+    protected function formattedRecords($records)
+    {
+
+        $results = [];
+
+        foreach ($records as $record)
+        {
+            $fields = [];
+
+            foreach ($this->tableColumns() as $column_name => $column)
+            {
+                $columnName = 'column_' . $column_name;
+
+                if (method_exists($this, $columnName))
+                {
+                    $content = $this->$columnName($record);
+                }
+                else
+                {
+
+                    $value   = $record->$column_name;
+                    $content = $value == null ? '&nbsp;' : $value;
+                }
+
+                $fields[] = $content;
+            }
+
+            $results[] = $fields;
+        }
+
+        return $results;
 
     }
 
@@ -475,12 +686,6 @@ abstract class Base
         return false;
     }
 
-    public function column($column_name)
-    {
-
-        return Arr::get($this->tableColumns(), $column_name);
-    }
-
     public function response()
     {
 
@@ -493,205 +698,11 @@ abstract class Base
     public function serverData()
     {
 
-        $record_count        = 0;
-        $record_filter_count = 0;
+        $data = $this->data();
 
-        #   Get the raw query records
-        $query = $this->query();
-
-        if ($query)
-        {
-            $record_count = $query->count();
-        }
-
-        # append the search query
-        $query = $this->querySearch($query);
-        $query = $this->querySetOrder($query);
-
-        if ($query)
-        {
-            $record_filter_count = $query->count();
-        }
-
-        $query = $this->queryLimit($query);
-
-        # take the search records and return the formatted result
-        $records = $this->querySearchRecords($query);
-
-        # take the formatted results and create the field elements.
-        $results = $this->formattedRecords($records);
-
-        $data['data']            = $results;
-        $data['draw']            = $this->search->draw;
-        $data['recordsTotal']    = $record_count;
-        $data['recordsFiltered'] = $record_filter_count;
+        $data['draw'] = $this->search->draw;
 
         return $data;
-    }
-
-    public function query()
-    {
-
-
-        return null;
-    }
-
-    /**
-     * @param $query
-     *              Return the query with the search filters appended to it.
-     * @return mixed
-     */
-    public function querySearch($query)
-    {
-
-
-        return $query;
-
-    }
-
-    public function querySetOrder($query)
-    {
-
-        $sort_directions = [
-            'asc'  => 'asc',
-            'desc' => 'desc'
-        ];
-
-        $order = $this->search->order;
-
-        foreach ($order as $order_item)
-        {
-
-            $column_number = Arr::get($order_item, 'column', '');
-
-            if (is_numeric($column_number))
-            {
-
-                $order_column = Arr::get($this->search->columns, $column_number);
-
-                if ($order_column)
-                {
-
-                    $column_name = Arr::get($order_column, 'name');
-
-                    if (empty($column_name))
-                    {
-                        continue;
-                    }
-
-                    $sort_direction = Arr::get($order_item, 'dir', 'asc');
-
-                    $sort_direction = Arr::get($sort_directions, $sort_direction, 'asc');
-
-                    $orderMethodName = 'order_' . $column_name;
-
-                    if (method_exists($this, $orderMethodName))
-                    {
-                        $content = $this->$orderMethodName($query, $sort_direction);
-                    }
-                    else
-                    {
-
-                        $column = $this->column($column_name);
-
-                        if ($column)
-                        {
-                            $sort_column = Arr::get($column, 'sort_field');
-
-                            if ($sort_column)
-                            {
-                                $query->orderBy($sort_column, $sort_direction);
-                            }
-
-                        }
-                    }
-                }
-            }
-
-        }
-
-        return $query;
-    }
-
-    public function queryLimit($query)
-    {
-
-        if ($query == null)
-        {
-            return $query;
-        }
-
-        $limit = $this->search->limit;
-
-        if (!empty($limit))
-        {
-            $query->limit($limit);
-        }
-
-        $start = $this->search->start;
-
-        if (!empty($start))
-        {
-            $query->offset($start);
-        }
-
-        return $query;
-
-    }
-
-    /**
-     * Retrieve the list of records used for this display, filtered by search variables.
-     *
-     * @return array
-     */
-    public function querySearchRecords($query_search)
-    {
-
-        if ($query_search)
-        {
-            return $query_search->get();
-        }
-
-        return [];
-    }
-
-    /**
-     * Retrieve the records formatted by their column_* calls.
-     *
-     * @return array
-     */
-    protected function formattedRecords($records)
-    {
-
-        $results = [];
-
-        foreach ($records as $record)
-        {
-            $fields = [];
-
-            foreach ($this->tableColumns() as $column_name => $column)
-            {
-                $columnName = 'column_' . $column_name;
-
-                if (method_exists($this, $columnName))
-                {
-                    $content = $this->$columnName($record);
-                }
-                else
-                {
-
-                    $value   = $record->$column_name;
-                    $content = $value == null ? '&nbsp;' : $value;
-                }
-
-                $fields[] = $content;
-            }
-
-            $results[] = $fields;
-        }
-
-        return $results;
-
     }
 
     public function scripts()
