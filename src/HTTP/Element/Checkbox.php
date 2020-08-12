db7ba07c467ea0b7199d788128232c141902ef99
Tony Black <tonyblack@outlook.com>
2020-08-12 16:37:56 -0600

    Updated checkbox fields.

--- src/HTTP/Element/Checkbox.php
+++ src/HTTP/Element/Checkbox.php
@@ -4,16 +4,19 @@ namespace SGT\HTTP\Element;
 
 use Form;
 
-class Checkbox extends Input
+class Checkbox extends Element
 {
 
     protected $type      = 'checkbox';
+    protected $type_file = 'checkbox';
 
     public function __construct()
     {
 
-        parent::__construct();
         $this->value(1);
+
+        $this->addClass('div', $this->configFrontEnd('element.checkbox.css.div'));
+        $this->addClass('element', $this->configFrontEnd('element.checkbox.css.element'));
 
     }
 
@@ -21,6 +24,30 @@ class Checkbox extends Input
     {
 
         $this->data('checked', $check);
+    }
+
+    public function draw()
+    {
+
+        if ($this->hasError())
+        {
+            $error_class = $this->configFrontEnd('element.css.error');
+            $this->addClass('element', $error_class);
+            $this->addClass('div', $error_class);
+        }
+
+        $element_view_path = $this->configFrontEnd('element.view.path');
+
+        $view_file = $this->getData('view_file');
+
+        $view_file = empty($view_file) ? $element_view_path : $view_file;
+
+        $view_file .= '/' . $this->type_file;
+
+        $view          = view($view_file);
+        $view->element = $this;
+
+        return $view->__toString();
     }
 
     public function drawElement()
